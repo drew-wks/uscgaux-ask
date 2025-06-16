9da412b00f8233e7f29283f927be679314155475
Drew Wilkins <20048187+drew-wks@users.noreply.github.com>
2025-06-16 13:38:58 -0400

    repo structuer cleanup

--- testing/playground/page_1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Page 1")
-st.session_state.unit
-st.page_link("streamlit_app.py", label="page_2") # this preserves state
--- testing/playground/page_2.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("Page 2")
-st.session_state.unit
-back = st.button("< Back to Page 1", type="primary")
-if back:
-    st.switch_page("page_1.py")
